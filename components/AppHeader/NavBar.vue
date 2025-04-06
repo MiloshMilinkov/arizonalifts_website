42f3647e008e75ba3c531c620858095e6e05f659
Milos Milinkov <milos300399@gmail.com>
2025-04-06 14:51:51 +0200

    Added a dropdown to the NavBar

--- components/AppHeader/NavBar.vue
+++ components/AppHeader/NavBar.vue
@@ -2,18 +2,28 @@
     <nav class="navbar">
       <div class="navbar-links">
         <NuxtLink to="/" class="nav-link">Početna</NuxtLink>
-        <NuxtLink to="/programs" class="nav-link">Programi</NuxtLink>
+        <div class="dropdown" @mouseenter="showDropdown = true" @mouseleave="showDropdown = false">
+          <NuxtLink to="/programs" class="nav-link">Programi</NuxtLink>
+          <div class="dropdown-menu" v-if="showDropdown">
+            <NuxtLink to="/programs" class="dropdown-item">Trening</NuxtLink>
+            <NuxtLink to="/nutrition" class="dropdown-item">Ishrana</NuxtLink>
+          </div>
+        </div>
       </div>
       <div class="navbar-logo">
         <h1>ARIZONA<span>LIFTS</span></h1>
       </div>
       <div class="navbar-links">
-        <a href="#" class="nav-link">O meni</a>
-        <a href="#" class="nav-link">Kontakt</a>
+        <NuxtLink to="/" class="nav-link">O Meni</NuxtLink>
+        <NuxtLink to="/programs" class="nav-link">Kontakt</NuxtLink>
       </div>
     </nav>
 </template>
-  
+
+<script setup>
+  import { ref } from 'vue';
+    const showDropdown = ref(false);
+</script>
   
 <style scoped>
   .navbar {
@@ -23,17 +33,19 @@
     width: 99%;
     z-index: 999;
     display: flex;
-    justify-content: space-between;
     align-items: center;
+    justify-content: center;
     background-color: #F4E1D2;
-    padding: 1rem 2rem;
-    height: 2rem;
+    padding-top: 1rem;
+    height: 3rem;
+    width: 100vw;
+    gap: 5rem;
   }
 
   
   .navbar-links {
     display: flex;
-    gap: 1.5rem;
+    gap: 5rem;
   }
   
   .nav-link {
@@ -45,8 +57,6 @@
     padding: 0.5rem 1rem;
     border-radius: 5px;
     font-family: inherit;
-    margin-right: 4rem;
-    margin-left: 2rem;
   }
   
   .nav-link:hover {
@@ -74,6 +84,34 @@
     background: linear-gradient(180deg,#F4E1D2 0%,#FADAC3 50%);
     color: #e76f51;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
+  }
+
+  .dropdown {
+    position: relative;
+  }
+
+  .dropdown-menu {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    background-color: #FADAC3;
+    padding: 0.5rem 0;
+    border-radius: 5px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.15);
+    min-width: 100%;
+  }
+
+  .dropdown-item {
+    display: block;
+    padding: 0.5rem 1rem;
+    color: #e76f51;
+    text-decoration: none;
+    transition: background 0.3s ease;
+  }
+
+  .dropdown-item:hover {
+    background: #f4a261;
+    color: #fff;
   }
   </style>
   
